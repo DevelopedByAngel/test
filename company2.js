ef5d8b5ef95574f5215670801e5605517a51ddc9
Angel Francis <50880391+DevelopedByAngel@users.noreply.github.com>
2020-07-27 17:11:12 +0530

    Update company2.js

--- company2.js
+++ company2.js
@@ -1,6 +1,8 @@
-import c, {name} from './company1.js'
+// import c, {name} from './company1.js'
 
-console.log(name)
-console.log(c.getName())
-c.setName("Mohtashim")
-console.log(c.getName())
+// console.log(name)
+// console.log(c.getName())
+// c.setName("Mohtashim")
+// console.log(c.getName())
+import {a as as}from './company1.js'
+as();
